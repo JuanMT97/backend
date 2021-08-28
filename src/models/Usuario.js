9a2794d2fc3be394a16ef2fffc83abf906377b65
JuanMT97 <50083109+JuanMT97@users.noreply.github.com>
2021-08-28 14:06:44 -0300

    Delete src directory

--- src/models/Usuario.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const UsuarioSchema = new Schema({
-    email: String,
-    admin: Boolean,
-});
-
-module.exports = model('Usuario', UsuarioSchema);
