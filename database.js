9323bebd1ee785c072a73a403ed9c49b1670c39f
JuanMT97 <50083109+JuanMT97@users.noreply.github.com>
2021-08-31 20:14:37 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,13 +1,5 @@
 const mongoose = require('mongoose');
 
-/*
-mongoose.connect('mongodb://localhost/tpf', {
-   useNewUrlParser: true
-})
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.log(err));
-*/
-
 mongoose.connect('mongodb+srv://Juan:1234@tpf.upkyw.mongodb.net/tpf?retryWrites=true&w=majority', {
    useNewUrlParser: true,
    useFindAndModify: false,
